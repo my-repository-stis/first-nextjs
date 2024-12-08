37075b5c063f20431a6b78694c3707c7ca33219e
Rio Siahaan <222112324@stis.ac.id>
2024-12-08 13:33:19 +0700

    3. Link and Navigation

---
+++ nextjs-tutorial/src/pages/auth/login.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+const LoginPage = () => {
+    const router = useRouter();
+    const handlerLogin = () => {
+        router.push("/product");
+    }
+    
+    return (
+        <div>
+            <h1>Login Page</h1>
+            <button onClick={() => handlerLogin()}>Login</button>
+            <p>Belum punya akun ? registrasi <Link href={"/auth/register"}>disini</Link></p>
+        </div>
+    )
+}
+
+export default LoginPage;
