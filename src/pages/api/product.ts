a8fa22b052a8d89d095ff30f2084a029c50a496d
Rio Siahaan <222112324@stis.ac.id>
2024-12-12 13:23:38 +0700

    6. API Routes

--- src/pages/api/product.ts
+++ src/pages/api/product.ts
@@ -1,33 +1,17 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { retrieveData } from '@/lib/firebase/service'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
     status : boolean,
     statusCode : number,
-  data : {
-      id : number,
-      name : string,
-      price : number,
-      size : string
-  }[]
+  data : any
 }
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-    const data = [{
-        id : 1,
-        name : "celana panjang",
-        price : 100000,
-        size : "l"
-    },
-    {
-        id : 2,
-        name : "kemeja",
-        price : 50000,
-        size : "m"
-    }
-    ]
+  const data = await retrieveData("products")
   res.status(200).json({ status: true, statusCode : 200, data })
 }
