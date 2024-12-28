f6e5b201c4eaabe59cc5e775daf08145abb081ba
Rio Siahaan <222112324@stis.ac.id>
2024-12-28 07:49:06 +0700

    Debugging error while deploying

--- src/views/detailProduct/index.tsx
+++ src/views/detailProduct/index.tsx
@@ -1,6 +1,7 @@
 import { productType } from "@/types/productType";
 import styles from "./DetailProduct.module.scss"
 import Link from "next/link";
+import Image from "next/image";
 
 const DetailProduct = ({product} : {product: productType}) => {
     return(
@@ -9,7 +10,8 @@ const DetailProduct = ({product} : {product: productType}) => {
             <div className={styles.productDetail}>
                 <Link href={`/product/${product.id}`} className={styles.productDetail__item} key={product.id}>
                     <div className={styles.productDetail__item__image}>
-                        <img src={product.image && product.image} alt={product.name} />
+                        {/* <img src={product.image && product.image} alt={product.name} /> */}
+                        <Image src={product.image && product.image} alt={product.name} width={200} height={100}/>
                     </div>
                     <h4 className={styles.productDetail__item__name}>
                         {product.name}
